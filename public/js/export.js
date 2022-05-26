928bed5b5fd02be3ca5c6bd97abed6c7cf38b5a2
ri-ca4 <riley.de12@gmail.com>
2022-05-25 22:21:49 -0400

    added dnd, genders, alignment, motivations, flaws, and personalities to export.js

---
+++ public/js/export.js
@@ -0,0 +1,38 @@
+const dnd = {
+
+    races: ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 
+        'Halfling', 'Half-Orc', 'Human', 'Tiefling'],
+
+    classes: ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 
+        'Monk', 'Palidin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 
+        'Wizard', 'Artificer', 'Blood Hunter']
+}
+
+const wow = {
+    races: [],
+    classes: []
+}
+
+const genders = ['Male', 'Female', 'Non-binary'];
+
+const alignment = [
+    ['Lawful', 'Neutral', 'Chaotic'],
+    ['Good', 'Neutral', 'Evil'] 
+];
+
+const motivations = ['revenge', 'power', 'money', 'healing', 'justice', 'entertainment',
+                     'love', 'adventure', 'education', 'respect', 'redemption', 'boredom',
+                     'curiosity', 'lust', 'anarchy', 'rebellion', 'survival', 'courage', 
+                     'glory', 'honor'];
+
+const flaws = ['gullible', 'clumbsy', 'lazy', 'naive', 'pride', 'stubborn', 'anxious', 'apathetic', 'ignorant',
+                'hot-headed', 'obsessive', 'distractable', 'arrogant', 'forgetful', 'compulsive liar'];
+
+const personality = ['bold', 'shy', 'quiet', 'serious', 'modest', 'brave', 'quirky', 'careful', 'honest', 
+                    'loyal', 'humble', 'responsible', 'combative', 'witty', 'simple minded', 'bright', 
+                    'impulsive', 'compassionate', 'leader', 'hard-working'];
+
+function statRoll (){
+
+}
+
